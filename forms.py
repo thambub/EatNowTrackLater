5e599c6b3323bbf3d778a33990b070b143bbe7f9
rdowney6161 <73893431+rdowney6161@users.noreply.github.com>
2020-11-12 15:00:17 -0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,9 +1,26 @@
 from django import forms
-from .models import Comment, Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
-class NewCommentForm(forms.ModelForm):
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
     class Meta:
-        model = Comment
-        fields = ['content']
+        model = User
+        fields = ['username', 'email', 'password1']
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'image', 'bio', 'favorite_food', 'favorite_hobby', 'user_age', 'user_height_in_inches', 'user_weight_in_lbs', 'user_sex']
--- models.py
+++ models.py
@@ -1,64 +1,269 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
 from PIL import Image
+from blog.models import Post
 from datetime import date
 
-
-
-
-class Post(models.Model):
-    content = models.TextField(max_length=1000)
-    servings_meat = models.IntegerField(default=0)
-    servings_dairy = models.IntegerField(default=0)
-    servings_grains = models.IntegerField(default=0)
-    servings_vegetables = models.IntegerField(default=0)
-    servings_fruit = models.IntegerField(default=0)
-    minutes_aerobic_exercise = models.IntegerField(default=0)
-    minutes_weightlifting_exercise = models.IntegerField(default=0)
-    image_post = models.ImageField(default='default.png', upload_to='post_pics')
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes= models.IntegerField(default=0)
-    dislikes= models.IntegerField(default=0)
-
-
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    user_age = models.IntegerField(default=0)
+    user_height_in_inches = models.IntegerField(default=0)
+    user_weight_in_lbs = models.IntegerField(default=0)
+    user_sex = models.TextField(default=0, max_length=10)
+    favorite_food = models.TextField(default=0, max_length=50)
+    favorite_hobby = models.TextField(default=0, max_length=50)
+    bio = models.TextField(default=0, max_length=50)
+    name = models.TextField(default=0, max_length=5)
 
     def __str__(self):
-        return self.content[:5]
+        return f'{self.user.username} Profile'
 
     @property
-    def number_of_comments(self):
-        return Comment.objects.filter(post_connected=self).count()
+    def followers(self):
+        return Follow.objects.filter(follow_user=self.user).count()
+
+    @property
+    def following(self):
+        return Follow.objects.filter(user=self.user).count()
     
+    def minutes_aerobic_activity_week(self):
+        today = date.today()
+        if today.weekday() == 0:
+            today = 0
+            return self.minutes_aerobic_activity_day(today)
+        elif today.weekday() == 1:
+            today = 0
+            days_before = 1
+            monday = int(self.minutes_aerobic_activity_day(days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday
+        elif today.weekday() == 2:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            monday = int(self.minutes_aerobic_activity_day(two_days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(days_before))
+            wednesday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday + wednesday
+        elif today.weekday() == 3:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            monday = int(self.minutes_aerobic_activity_day(three_days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(two_days_before))
+            wednesday = int(self.minutes_aerobic_activity_day(days_before))
+            thursday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday + wednesday + thursday
+        elif today.weekday() == 4:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            monday = int(self.minutes_aerobic_activity_day(four_days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(three_days_before))
+            wednesday = int(self.minutes_aerobic_activity_day(two_days_before))
+            thursday = int(self.minutes_aerobic_activity_day(days_before))
+            friday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday
+        elif today.weekday() == 5:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            five_days_before = 5
+            monday = int(self.minutes_aerobic_activity_day(five_days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(four_days_before))
+            wednesday = int(self.minutes_aerobic_activity_day(three_days_before))
+            thursday = int(self.minutes_aerobic_activity_day(two_days_before))
+            friday = int(self.minutes_aerobic_activity_day(days_before))
+            saturday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday + saturday
+        elif today.weekday() == 6:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            five_days_before = 5
+            six_days_before = 6
+            monday = int(self.minutes_aerobic_activity_day(six_days_before))
+            tuesday = int(self.minutes_aerobic_activity_day(five_days_before))
+            wednesday = int(self.minutes_aerobic_activity_day(four_days_before))
+            thursday = int(self.minutes_aerobic_activity_day(three_days_before))
+            friday = int(self.minutes_aerobic_activity_day(two_days_before))
+            saturday = int(self.minutes_aerobic_activity_day(days_before))
+            sunday = int(self.minutes_aerobic_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday + saturday + sunday
+        
+    def minutes_weightlifting_activity_week(self):
+        today = date.today()
+        if today.weekday() == 0:
+            today = 0
+            return self.minutes_weightlifting_activity_day(today)
+        elif today.weekday() == 1:
+            today = 0
+            days_before = 1
+            monday = int(self.minutes_weightlifting_activity_day(days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday
+        elif today.weekday() == 2:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            monday = int(self.minutes_weightlifting_activity_day(two_days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(days_before))
+            wednesday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday + wednesday
+        elif today.weekday() == 3:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            monday = int(self.minutes_weightlifting_activity_day(three_days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(two_days_before))
+            wednesday = int(self.minutes_weightlifting_activity_day(days_before))
+            thursday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday + wednesday + thursday
+        elif today.weekday() == 4:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            monday = int(self.minutes_weightlifting_activity_day(four_days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(three_days_before))
+            wednesday = int(self.minutes_weightlifting_activity_day(two_days_before))
+            thursday = int(self.minutes_weightlifting_activity_day(days_before))
+            friday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday
+        elif today.weekday() == 5:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            five_days_before = 5
+            monday = int(self.minutes_weightlifting_activity_day(five_days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(four_days_before))
+            wednesday = int(self.minutes_weightlifting_activity_day(three_days_before))
+            thursday = int(self.minutes_weightlifting_activity_day(two_days_before))
+            friday = int(self.minutes_weightlifting_activity_day(days_before))
+            saturday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday + saturday
+        elif today.weekday() == 6:
+            today = 0
+            days_before = 1
+            two_days_before = 2
+            three_days_before = 3
+            four_days_before = 4
+            five_days_before = 5
+            six_days_before = 6
+            monday = int(self.minutes_weightlifting_activity_day(six_days_before))
+            tuesday = int(self.minutes_weightlifting_activity_day(five_days_before))
+            wednesday = int(self.minutes_weightlifting_activity_day(four_days_before))
+            thursday = int(self.minutes_weightlifting_activity_day(three_days_before))
+            friday = int(self.minutes_weightlifting_activity_day(two_days_before))
+            saturday = int(self.minutes_weightlifting_activity_day(days_before))
+            sunday = int(self.minutes_weightlifting_activity_day(today))
+            return monday + tuesday + wednesday + thursday + friday + saturday + sunday
+        
+         
+            
+
+    def minutes_aerobic_activity_day(self,days_before):
+        today = date.today()
+        posts_on_day = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day-days_before, author=self.user)
+        total_servings = 0
+        for post in posts_on_day:
+            total_servings = total_servings + post.minutes_aerobic_exercise
+        return total_servings
+    
+    def minutes_weightlifting_activity_day(self,days_before):
+        today = date.today()
+        posts_on_day = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day-days_before, author=self.user)
+        total_servings = 0
+        for post in posts_on_day:
+            total_servings = total_servings + post.minutes_weightlifting_exercise
+        return total_servings
+    
+    def minutes_aerobic_activity_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.minutes_aerobic_exercise
+        return total_servings
+    
+    def minutes_weightlifting_activity_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.minutes_weightlifting_exercise
+        return total_servings
+    
+    def meat_servings_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.servings_meat
+        return total_servings
+    
+    def grains_servings_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.servings_grains
+        return total_servings
+
+    def dairy_servings_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.servings_dairy
+        return total_servings
+
+    def vegetables_servings_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.servings_vegetables
+        return total_servings
+
+    def fruit_servings_today(self):
+        today = date.today()
+        posts_today = Post.objects.filter(date_posted__year=today.year, date_posted__month=today.month, date_posted__day=today.day, author=self.user)
+        total_servings = 0
+        for post in posts_today:
+            total_servings = total_servings + post.servings_fruit
+        return total_servings
+
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         super().save()
 
-        img_post = Image.open(self.image_post.path)
-        if img_post.height > 600 or img_post.width > 600:
-            output_size = (600, 600)
-            img_post.thumbnail(output_size)
-            img_post.save(self.image_post.path)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
-
-class Comment(models.Model):
-    content = models.TextField(max_length=150)
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_connected = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-
-class Preference(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    post= models.ForeignKey(Post, on_delete=models.CASCADE)
-    value= models.IntegerField()
-    date= models.DateTimeField(auto_now= True)
-
-    def __str__(self):
-        return str(self.user) + ':' + str(self.post) +':' + str(self.value)
-
-    class Meta:
-       unique_together = ("user", "post", "value")
+class Follow(models.Model):
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    follow_user = models.ForeignKey(User, related_name='follow_user', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+     
+   
+        
+    
+    
